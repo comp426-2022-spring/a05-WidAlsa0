86d352b30f9c28a6c364a33c84d85ed8573950a9
WidAlsa0 <69405330+WidAlsa0@users.noreply.github.com>
2022-04-30 17:16:38 -0400

    creating functions for logging middleware + allowing HTML

--- index.js
+++ index.js
@@ -32,19 +32,15 @@ const port = allArguments['port'] || process.env.PORT || DEFAULT_PORT
 const express = require('express')
 const app = express()
 
-// Start an app server
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 })
 
-// Serve static HTML files
 app.use(express.static('./public'));
 
-// Make Express use its own built-in body parser for both urlencoded and JSON body data.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Logging middleware
 app.use(require('./src/middleware/logging.js'))
 
 // Additional (combined format) logging middleware, if log is true
@@ -60,8 +56,6 @@ if (allArguments['log'] == true) {
 }
 
 
-/***** API endpoints *****/
-// Check endpoint
 app.get('/app/', (req, res, next) => {
     res.status(HTTP_STATUS_OK).json({
         'message': "Your API works! (" + HTTP_STATUS_OK + ")"
@@ -71,19 +65,15 @@ app.get('/app/', (req, res, next) => {
 //Coin-flipping
 app.use(require("./src/routes/flipRoutes"))
 
-// Logging and error testing, if debug is true
 if (allArguments['debug'] == true) {
     app.use(require("./src/routes/debugRoutes"))
 }
 
-// Default response for any request not addressed by the defined endpoints
 app.use(function (req, res, next) {
     res.json({ "message": "Endpoint not found. (404)" });
     res.status(HTTP_STATUS_NOT_FOUND);
 });
 
-
-/***** Closing server *****/
 process.on('SIGTERM', () => {
     server.close(() => {
         console.log('Server stopped')
